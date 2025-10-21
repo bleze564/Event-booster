b7c24f4e10984683a1d76319516cd6b647d10606
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-10-21 20:37:58 +0300

    132

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const formApi = document.querySelector('form');
 const divApi = document.querySelector('.api-main');
 
-formApi.addEventListener('submit', () => {
-  // 1.отримати значення інпуту
+formApi.addEventListener('submit', ev => {
+  ev.preventDefault();
+  const searchValue = ev.target.elements.query.value;
+
   // 2.викликати функцію searcharticlesbyname як аргумент значення інпуту
-  
+  searchEventsByName(searchValue).then(result => {
+    let markup = result._embedded.events.map(createEventMarkup);
+  });
 });
 
 function searchEventsByName(queryFound) {
