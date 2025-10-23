7bd9b54bf935c186b4c021575c36da89ad76daa3
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-10-23 20:34:23 +0300

    123

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
+fetchEvents().then(result => {
+    let markup = result._embedded.events.map(createEventMarkup);
+    divApi.insertAdjacentHTML("beforeend", markup)
+  })
 const formApi = document.querySelector('form');
 const divApi = document.querySelector('.api-main');
 
 formApi.addEventListener('submit', ev => {
   ev.preventDefault();
   const searchValue = ev.target.elements.query.value;
-
-  // 2.викликати функцію searcharticlesbyname як аргумент значення інпуту
   searchEventsByName(searchValue).then(result => {
-    let markup = result.embedded.events.map(createEventMarkup);
+    let markup = result._embedded.events.map(createEventMarkup);
+    divApi.insertAdjacentHTML("beforeend", markup)
   });
 });
+function fetchEvents(){
+  return fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04`
+  ).then(result => result.json());
+}
 
 function searchEventsByName(queryFound) {
   return fetch(
