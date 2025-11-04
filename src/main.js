95089082c8a2b9f8ca9ec940a598ea4b52ab0a9a
bleze564 <153006729+bleze564@users.noreply.github.com>
2025-11-04 18:33:56 +0200

    123123

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+import Pagination from 'tui-pagination';
+
+let page = 0;
+
 fetchEvents().then(result => {
+  page += 1;
   let markup = result._embedded.events.map(createEventMarkup);
   divApi.insertAdjacentHTML('beforeend', markup);
 });
 const formApi = document.querySelector('form');
 const divApi = document.querySelector('.api-main');
-
+const paginationContainer = document.getElementById('pagination');
 formApi.addEventListener('submit', ev => {
   ev.preventDefault();
   const searchValue = ev.target.elements.query.value;
@@ -13,9 +18,9 @@ formApi.addEventListener('submit', ev => {
     divApi.innerHTML = markup;
   });
 });
-function fetchEvents() {
+function fetchEvents(page = '0') {
   return fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04`
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04?page=${page}`
   ).then(result => result.json());
 }
 
@@ -37,3 +42,34 @@ function createEventMarkup(event) {
      </article>
 `;
 }
+
+// пагінація
+
+const pagination = new Pagination(paginationContainer, {
+  totalItems: 20,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+});
+pagination.on('afterMove', function (eventData) {
+  fetchEvents(page).then(result => {
+    page += 1;
+    let markup = result._embedded.events.map(createEventMarkup);
+    divApi.insertAdjacentHTML('beforeend', markup);
+  });
+  alert('The current page is ' + eventData.page);
+});
